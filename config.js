48f7223e718f82632c7dffe834b00e54bab98c4d
M <cakenumber1@mail.ru>
2021-04-18 16:48:17 +0300

    port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 27665,
     url: 'mongodb+srv://user1:Password1forUser1@cluster0.s0sjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
